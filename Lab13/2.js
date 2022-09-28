17353f82d0faf987f1c0b0a1a83ad895da9ded35
maksuserv <106485827+maksuserv@users.noreply.github.com>
2022-09-28 22:42:40 +0300

    Update 2.js

--- Lab13/2.js
+++ Lab13/2.js
@@ -1,32 +1,32 @@
-let numbers = [2, 2, 8, 5, 6, 8, 2];
-let number = 8;
+let numbers = [1, 2, 1, 4, 1];
+let number = 1;
 
 let lastIndex = -1;
-for(let index = 0; index < numbers.length; index++){
-    if(number === numbers[index]){
-        lastIndex = index;
-    }
+for (let index = 0; index < numbers.length; index++) {
+  if (number === numbers[index]) {
+    lastIndex = index;
+  }
 }
 
 alert(lastIndex);
 
-for(let i = 0; i <= numbers.length - 2; i++){
-    let minValue = numbers[i];
-    for(let j = i + 1; j <= numbers.length - 1; j++){
-        if(numbers[j] < minValue){
-            minValue = numbers[j];
-            let swap = numbers[i];
-            numbers[i] = minValue;
-            numbers[j] = swap;
-        }
+for (let i = 0; i <= numbers.length - 2; i++) {
+  let minValue = numbers[i];
+  for (let j = i + 1; j <= numbers.length - 1; j++) {
+    if (numbers[j] < minValue) {
+      minValue = numbers[j];
+      let swap = numbers[i];
+      numbers[i] = minValue;
+      numbers[j] = swap;
     }
+  }
 }
 
 lastIndex = "элемент не найден";
-for(let index = 0; index < numbers.length; index++){
-    if(number === numbers[index]){
-        lastIndex = index;
-    }
+for (let index = 0; index < numbers.length; index++) {
+  if (number === numbers[index]) {
+    lastIndex = index;
+  }
 }
 
 console.log(lastIndex);
