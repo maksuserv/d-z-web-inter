466cfcfa77446d7292d2a7fc4acc8648b03077ef
maksuserv <106485827+maksuserv@users.noreply.github.com>
2022-09-28 22:43:26 +0300

    Update 3.js

--- Lab13/3.js
+++ Lab13/3.js
@@ -1,11 +1,11 @@
-let calculateSalary = function(dirty){
-    if(dirty < 100000){
-        return dirty - dirty / 100 * 35;
-    }
+let calculateSalary = function (dirty) {
+  if (dirty < 100000) {
+    return dirty - (dirty / 100) * 35;
+  }
 
-    return dirty - dirty / 100 * 45;
+  return dirty - (dirty / 100) * 45;
 };
 
-console.log(Math.round(calculateSalary(65000)));
+console.log(Math.round(calculateSalary(50000)));
 console.log(Math.round(calculateSalary(80000)));
-console.log(Math.round(calculateSalary(100000)));
+console.log(Math.round(calculateSalary(110000)));
