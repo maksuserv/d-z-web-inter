761a5788d81582efcbab84b7a9e6df4138d71621
maksuserv <106485827+maksuserv@users.noreply.github.com>
2022-09-28 22:37:47 +0300

    Update 2.js

--- Lab12/2.js
+++ Lab12/2.js
@@ -1,37 +1,37 @@
 let isRaining = confirm("Идёт дождь?");
 
 let minutes;
-if(isRaining){
-    minutes = 0;
+if (isRaining) {
+  minutes = 0;
 } else {
-    let temperature = prompt("Какая температура на улице?", "0");
-    if(temperature >= 10 && temperature < 15){
-        minutes = 30;
+  let temperature = prompt("Какая температура на улице?", "0");
+  if (temperature >= 10 && temperature < 15) {
+    minutes = 30;
+  } else {
+    if (temperature >= 15 && temperature < 25) {
+      minutes = 40;
     } else {
-        if(temperature >= 15 && temperature < 25){
-            minutes = 40;
-        } else {
-            if(temperature >= 25 && temperature <= 35){
-                minutes = 20;
-            } else {
-                minutes = 0;
-            }
-        }
+      if (temperature >= 25 && temperature <= 35) {
+        minutes = 20;
+      } else {
+        minutes = 0;
+      }
     }
+  }
 }
 
 console.log("Длительность прогулки: " + minutes + " минут");
 
 isRaining = confirm("Идёт дождь?");
-if(isRaining){
-    minutes = 0;
+if (isRaining) {
+  minutes = 0;
 } else {
-    temperature = prompt("Какая температура на улице?", "0");
-    if(temperature < 0 || temperature > 35){
-        minutes = 0;
-    } else {
-        minutes = 20 - Math.abs(20 - temperature);
-    }
+  temperature = prompt("Какая температура на улице?", "0");
+  if (temperature < 0 || temperature > 35) {
+    minutes = 0;
+  } else {
+    minutes = 20 - Math.abs(20 - temperature);
+  }
 }
 
 alert("Длительность прогулки: " + minutes + " минут");
