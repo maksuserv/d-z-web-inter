19f179592778d53744d12ce4b1e2a7356745fbaf
maksuserv <106485827+maksuserv@users.noreply.github.com>
2022-09-28 22:36:52 +0300

    Update 1.js

--- Lab12/1.js
+++ Lab12/1.js
@@ -1,18 +1,18 @@
 let age = prompt("Укажите возраст", "0");
 
 let ageGroup;
-if(age <= 1){
-    ageGroup = "Щенки";
+if (age <= 1) {
+  ageGroup = "Щенки";
 } else {
-    if(age > 1 && age <= 3){
-        ageGroup = "Молодые собаки";
+  if (age > 1 && age <= 3) {
+    ageGroup = "Молодые собаки";
+  } else {
+    if (age > 3 && age <= 7) {
+      ageGroup = "Собаки средних лет";
     } else {
-        if(age > 3 && age <= 7){
-            ageGroup = "Собаки средних лет";
-        } else {
-            ageGroup = "Почтенные таксоны";
-        }
+      ageGroup = "Почтенные таксоны";
     }
+  }
 }
 
 alert(ageGroup);
