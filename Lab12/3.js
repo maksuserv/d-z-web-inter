ad6ed5e685f6e2a77b9a5a04a91a5db7039634c1
maksuserv <106485827+maksuserv@users.noreply.github.com>
2022-09-28 22:38:32 +0300

    Update 3.js

--- Lab12/3.js
+++ Lab12/3.js
@@ -3,16 +3,23 @@ let time = prompt("Сколько часов?", "0");
 let goToDairy = false;
 let goToStore = false;
 let goToMarket = false;
-if((time >= 8 && time < 13) || (time >= 14 && time < 19)){
-    goToDairy = true;
+if ((time >= 8 && time < 13) || (time >= 14 && time < 19)) {
+  goToDairy = true;
 } else {
-    if((time >= 9 && time < 14) || (time >= 15 && time < 17)){
-        goToStore = true;
-    } else {
-        if(time >= 7 && time < 20){
-            goToMarket = true;
-        }
+  if ((time >= 9 && time < 14) || (time >= 15 && time < 17)) {
+    goToStore = true;
+  } else {
+    if (time >= 7 && time < 20) {
+      goToMarket = true;
     }
+  }
 }
 
-alert("goToDairy: " + goToDairy + "\ngoToStore: " + goToStore + "\ngoToMarket: " + goToMarket);
+alert(
+  "goToDairy: " +
+    goToDairy +
+    "\ngoToStore: " +
+    goToStore +
+    "\ngoToMarket: " +
+    goToMarket
+);
